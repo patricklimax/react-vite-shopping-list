1a2f404c638498baca54037721f8580464d037fc
patricklimax <patricklimax@outlook.com.br>
2024-09-25 15:12:49 -0300

    chore: update type on the Product

--- src/types/product.ts
+++ src/types/product.ts
@@ -4,9 +4,9 @@ export type Product = {
   id: string;
   name: string;
   isChecked: boolean;
-  quantity: number;
-  measure?: string;
-  category?: string;
+  quantity: string;
+  measure: string;
+  category: string;
   check?: ReactNode;
   edit?: ReactNode;
   remove?: ReactNode;
